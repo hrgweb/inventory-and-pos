4f9a7b12cf94a7a3623a9449417812d4c07fa88d
hrgweb <gamayonhergenroque.web@gmail.com>
2024-11-17 21:29:55 +0800

    install nuxt/ui and tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Home</h2>
+
+    <UButton label="Button" @click="onClick" />
+  </div>
+</template>
+
+<script setup lang="ts">
+function onClick() {
+  alert('click me')
+}
+</script>
